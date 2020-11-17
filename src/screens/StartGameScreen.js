3f7c75ad42959eaeb805e86a946f6e9364f06d5c
zhydani <zhydani@gmail.com>
2020-11-17 15:10:53 -0300

    adicionando shadow e uma borda para o quadro do input

--- src/screens/StartGameScreen.js
+++ src/screens/StartGameScreen.js
@@ -30,7 +30,19 @@ const styles = StyleSheet.create({
     inputContainer: {
         width: 300,
         maxWidth: '80%',
-        alignItems: 'center'
+        alignItems: 'center',
+        // shadow funciona apenas no iphone
+        shadowColor: 'black',
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowRadius: 6, 
+        shadowOpacity: 0.26,
+        backgroundColor: 'white',
+        elevation: 5, // para funcionar no android,
+        padding: 20,
+        borderRadius: 10
     },
     buttonContainer: {
         flexDirection: 'row',
