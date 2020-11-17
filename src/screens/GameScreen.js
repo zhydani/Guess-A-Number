207fffb955651447e509165100b07ba75d71a9a6
zhydani <zhydani@gmail.com>
2020-11-17 18:27:57 -0300

    adicionando a tela de game

---
+++ src/screens/GameScreen.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const generateRandomBetween = (min, max, exclude) => {
+    
+}
+
+const GameScreen = props => {
+    return(
+        <View>
+
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default GameScreen;
