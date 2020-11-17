a394a3b4f68c06bc1469578b563e88ceedf204e1
zhydani <zhydani@gmail.com>
2020-11-17 12:20:46 -0300

    adicionando layout do jogo

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import {View, StyleSheet} from 'react-native';
+
 import Header from './src/components/Header';
+import StartGameScreen from './src/screens/StartGameScreen'
 
 export default function App() {
   return (
     <View style={styles.screen}>
       <Header title="Guess a Number" />
+      <StartGameScreen />
     </View>
   )
 }
